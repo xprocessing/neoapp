f76cc47c8db508ec1d8155257acefd46e7380508
Neo <xprocessing@qq.com>
2026-06-16 23:20:35 +0800

    Delete admin/node_modules directory

--- admin/node_modules/tdesign-vue-next/cjs/common/docs/plugins/changelog-to-json/libs.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Platform } from './types';
-export declare const convert2PascalCase: (name: string) => string;
-export declare const COMPONENT_MAPS: {
-    web: Record<string, string[]>;
-    mobile: Record<string, string[]>;
-    chat: Record<string, string[]>;
-};
-export declare const mapToParentName: (name: string, platform: Platform) => string;
--- admin/node_modules/tdesign-vue-next/es/common/js/global-config/mobile/default-config.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    readonly classPrefix: "t";
-    readonly form: {
-        readonly requiredMark: any;
-    };
-};
-export default _default;
